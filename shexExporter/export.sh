fa1523ff740f1009db47086e6af281750cc3f4db
Lucas Werkmeister <mail@lucaswerkmeister.de>
2018-05-30 15:45:37 +0200

    Prefix all Jena binaries with "jena."
    On Arch Linux, a tdbdump binary is provided by the tdb package, a
    dependency of pulseaudio and other packages (“trivial database”),
    completely distinct from the Jena binary that originally bears the same
    name. The AUR Jena package [1] resolves this conflict by prefixing all
    binary names with "jena.", and only providing prefix-less symlinks for
    those programs which don’t conflict.
    
    [1]: https://aur.archlinux.org/packages/apache-jena/

--- shexExporter/export.sh
+++ shexExporter/export.sh
@@ -24,17 +24,17 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 echo "exporting"
 rm -rf ./temp
 mkdir temp
-tdbquery --loc $project --query $DIR/queries/all.txt --results N3 > temp/temp1.n3
-tdbquery --loc $project --query $DIR/queries/createRoot.txt --results N3 > temp/temp2.n3
-tdbloader -loc ./temp/tempdb --graph=http://ssb.wur.nl/shexExporter temp/temp1.n3
-tdbloader -loc ./temp/tempdb --graph=http://ssb.wur.nl/shexExporter temp/temp2.n3
-tdbupdate -loc ./temp/tempdb --update $DIR/queries/removeSubClassOfThing.txt
+jena.tdbquery --loc $project --query $DIR/queries/all.txt --results N3 > temp/temp1.n3
+jena.tdbquery --loc $project --query $DIR/queries/createRoot.txt --results N3 > temp/temp2.n3
+jena.tdbloader -loc ./temp/tempdb --graph=http://ssb.wur.nl/shexExporter temp/temp1.n3
+jena.tdbloader -loc ./temp/tempdb --graph=http://ssb.wur.nl/shexExporter temp/temp2.n3
+jena.tdbupdate -loc ./temp/tempdb --update $DIR/queries/removeSubClassOfThing.txt
 #hack not to use to much memory
-while [[ $(tdbquery -loc ./temp/tempdb/ --query $DIR/queries/removeSubClassOfMeshDone.txt | grep '[01]' |sed 's/.*\([01]\).*/\1/') -eq 1 ]] ; do
-  tdbupdate -loc ./temp/tempdb --update $DIR/queries/removeSubClassOfMesh.txt
+while [[ $(jena.tdbquery -loc ./temp/tempdb/ --query $DIR/queries/removeSubClassOfMeshDone.txt | grep '[01]' |sed 's/.*\([01]\).*/\1/') -eq 1 ]] ; do
+  jena.tdbupdate -loc ./temp/tempdb --update $DIR/queries/removeSubClassOfMesh.txt
   echo "."
 done
-tdbdump --loc ./temp/tempdb > temp/temp.n4
+jena.tdbdump --loc ./temp/tempdb > temp/temp.n4
 $DIR/jsonconvert.js convert ./temp/temp.n4  > temp/out.json
 $DIR/jsonconvert.js compact temp/out.json -c $DIR/context.json > temp/compact.json
 $DIR/jsonconvert.js frame temp/compact.json -f $DIR/frame.json > temp/result.json

14b6d85edd7635c54043be0f12c3aa150cf8007d
Lucas Werkmeister <mail@lucaswerkmeister.de>
2018-06-01 12:05:26 +0200

    Link RDF2Graph.jar instead of copying it
    Otherwise, the RDF2Graph.jar file in the repository root will be stale
    after subsequent rebuilds.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ cd RDFSimpleCon
 mvn install
 cd ..
 mvn install
-cp ./target/RDF2Graph-0.1-jar-with-dependencies.jar ./RDF2Graph.jar
+ln -s ./target/RDF2Graph-0.1-jar-with-dependencies.jar ./RDF2Graph.jar
 cd shexExporter
 npm install
